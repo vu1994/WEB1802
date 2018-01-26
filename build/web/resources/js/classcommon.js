1eac990af64f116fd18be603d3c6fa8107d14eba
tranhoangyen <tranhoangyen2802@gmail.com>
2018-01-26 22:32:30 +0700

    Thêm dashboardMain, và hiệu ứng hover
    Thêm dashboardMain, và hiệu ứng hover

---
+++ build/web/resources/js/classcommon.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function verifyDelete(){
+    if(confirm('Bạn có chắc chắn xóa dữ liệu này !')){
+        return true;
+    } else {
+        return false;
+    }
+}
