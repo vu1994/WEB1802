9a35ab01cfd2e7a06c47359b937511dd37a0e7df
vu1994 <vub1203987@student.ctu.edu.vn>
2018-01-28 18:57:07 +0700

    Chức năng quản lý thiết bi
    - Hiển thị danh sách thiết bị
    - Cập nhật form thêm thiết bị
    - Sửa lỗi Validate

--- src/java/com/cusc/controller/QuanLyThietBiController.java
+++ src/java/com/cusc/controller/QuanLyThietBiController.java
@@ -5,6 +5,13 @@
  */
 package com.cusc.controller;
 
+import com.cusc.dataprovider.QuanLyThietBiDataProvider;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -15,20 +22,102 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean (name="QuanLyThietBiController")
 @ViewScoped
 public class QuanLyThietBiController {
-    
+    QuanLyThietBiDataProvider thietBiDp = new QuanLyThietBiDataProvider();
+    private List<Map> listThietBi = new ArrayList<>();
+    private int selectedNhomThietBi;
+    private int selectedTinhTrang;
+    private Date selectedNgayNhap;
+    private int selectedNhanVien;
+    private boolean selectedCapPhat = false;
     private int viewMode = 0;
     /**
      * Creates a new instance of QuanLyThietBiController
      */
     public QuanLyThietBiController() {
+        listThietBi.clear();
+        this.actionGetListThietBi();
     }
-
+    
+    public void actionGetListThietBi(){
+        listThietBi = thietBiDp.getListThietBi();
+    }
+    
+    public void preActionThemThietBi(){
+        Calendar currentDate = Calendar.getInstance();
+        selectedNgayNhap = currentDate.getTime();
+        viewMode = 1;
+    }
+    
+    public void actionThemThietBi(){
+        
+    }
+    
+    public void showFormCapPhat(){
+        System.out.println("npvu: "+selectedCapPhat);
+        selectedCapPhat = !selectedCapPhat;
+    }
+    
+    public void actionTroVe(){
+        viewMode = 0;
+    }
+            
+            
+    // Getter & Setter
+    
     public int getViewMode() {
         return viewMode;
     }
 
     public void setViewMode(int viewMode) {
         this.viewMode = viewMode;
+    }
+
+    public List<Map> getListThietBi() {
+        return listThietBi;
+    }
+
+    public void setListThietBi(List<Map> listThietBi) {
+        this.listThietBi = listThietBi;
+    }
+
+    public int getSelectedNhomThietBi() {
+        return selectedNhomThietBi;
+    }
+
+    public void setSelectedNhomThietBi(int selectedNhomThietBi) {
+        this.selectedNhomThietBi = selectedNhomThietBi;
+    }
+
+    public int getSelectedTinhTrang() {
+        return selectedTinhTrang;
+    }
+
+    public void setSelectedTinhTrang(int selectedTinhTrang) {
+        this.selectedTinhTrang = selectedTinhTrang;
+    }
+
+    public int getSelectedNhanVien() {
+        return selectedNhanVien;
+    }
+
+    public void setSelectedNhanVien(int selectedNhanVien) {
+        this.selectedNhanVien = selectedNhanVien;
+    }
+
+    public Date getSelectedNgayNhap() {
+        return selectedNgayNhap;
+    }
+
+    public void setSelectedNgayNhap(Date selectedNgayNhap) {
+        this.selectedNgayNhap = selectedNgayNhap;
+    }
+
+    public boolean isSelectedCapPhat() {
+        return selectedCapPhat;
+    }
+
+    public void setSelectedCapPhat(boolean selectedCapPhat) {
+        this.selectedCapPhat = selectedCapPhat;
     }
     
 }
---
+++ src/java/com/cusc/dataprovider/QuanLyThietBiDataProvider.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cusc.dataprovider;
+
+import com.cusc.util.HibernateUtil;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.Session;
+import org.hibernate.transform.Transformers;
+
+/**
+ *
+ * @author SunnyNguyen
+ */
+public class QuanLyThietBiDataProvider implements Serializable{
+
+    public List<Map> getListThietBi(){
+        Session session = HibernateUtil.currentSession();
+        List<Map> listThietBi = new ArrayList();
+        try {
+            session.beginTransaction();
+            listThietBi = session.createSQLQuery("SELECT tb.*, dmtb.danhmuc_thietbi_ten, tt.tinhtrang_ten"
+                    + " FROM thietbi tb"
+                    + " LEFT JOIN danhmuc_thietbi dmtb ON tb.danhmuc_thietbi_id = dmtb.danhmuc_thietbi_id"
+                    + " LEFT JOIN tinhtrang tt ON tb.tinhtrang_id = tt.tinhtrang_id"
+                    + "  ").setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+            session.getTransaction().commit();
+            int stt = 1;
+            for(Map mapThietBi : listThietBi){
+                mapThietBi.put("rowIndex", stt);
+                stt++;
+            }
+	} catch (Exception e) {
+            e.printStackTrace();
+	} finally {
+            session.close();
+	}
+        return listThietBi;
+    }
+}
