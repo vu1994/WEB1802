957cdf380ffca51ef2c3dcc943b96f028b32d65e
tranhoangyen <tranhoangyen2802@gmail.com>
2018-01-29 17:12:19 +0700

    giao diện

--- src/java/com/cusc/controller/NhanVienController.java
+++ src/java/com/cusc/controller/NhanVienController.java
@@ -6,7 +6,7 @@
 package com.cusc.controller;
 
 import com.cusc.dataprovider.NhanVienProvider;
-import com.cusc.model.DanhMucThietBiModel;
+import com.cusc.model.NhanVienModel;
 import java.util.List;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
@@ -22,6 +22,23 @@ public class NhanVienController {
     private NhanVienProvider nvProvider = new NhanVienProvider();
     private List<Map> listDmNhanVien;
     private int selectedPhongBan;
+    private NhanVienModel objNhanVien = new NhanVienModel();
+
+    public NhanVienProvider getNvProvider() {
+        return nvProvider;
+    }
+
+    public void setNvProvider(NhanVienProvider nvProvider) {
+        this.nvProvider = nvProvider;
+    }
+
+    public NhanVienModel getObjNhanVien() {
+        return objNhanVien;
+    }
+
+    public void setObjNhanVien(NhanVienModel objNhanVien) {
+        this.objNhanVien = objNhanVien;
+    }
    
     
     public NhanVienController(){
